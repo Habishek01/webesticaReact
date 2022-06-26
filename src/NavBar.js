3ec4272d6c0d5f09fa0db6fb00d4deb7e2699fe4
Abishek <abishekabi710@gmail.com>
2022-06-26 10:00:29 +0530

    New navbar css

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,56 +1,56 @@
+import "./navBar.css";
+
 export default function NavBar() {
   return (
-    <div>
-      <div className="fnavBar">
-        <div id="navBar">
-          <div id="navBar1">
-            <img
-              alt=""
-              src="https://social.webestica.com/assets/images/logo.svg"
+    <div className="navBar">
+      <div className="container">
+        <div className="leftContainer">
+          <img
+            alt=""
+            src="https://social.webestica.com/assets/images/logo.svg"
+          />
+          <form className="search-container">
+            <span className="material-symbols-outlined"> search </span>
+            <input
+              className="search"
+              type="search"
+              placeholder="Search..."
+              aria-label="Search"
             />
-            <form className="input-container">
-              <span className="material-symbols-outlined"> search </span>
-              <input
-                className="input"
-                type="search"
-                placeholder="Search..."
-                aria-label="Search"
-              />
-            </form>
+          </form>
+        </div>
+
+        <div className="rightContainer">
+          <div className="link">
+            <span>Demo</span>
+            <span className="material-icons dropDown"> expand_more </span>
           </div>
-          <div id="navBar2">
-            <div className="link">
-              <span>Demo</span>
-              <span className="material-icons"> expand_more </span>
-            </div>
-            <div className="link">
-              <span>Pages</span>
-              <span className="material-icons"> expand_more </span>
-            </div>
-            <div className="link">
-              <span>Account</span>
-              <span className="material-icons"> expand_more </span>
-            </div>
-            <div className="link">
-              <span>My Network</span>
-            </div>
+          <div className="link">
+            <span>Pages</span>
+            <span className="material-icons dropDown"> expand_more </span>
           </div>
-          <div id="navBar3">
-            <button className="button">
-              <span className="material-icons"> chat </span>
-            </button>
-            <button className="button">
-              <span className="material-icons"> settings </span>
-            </button>
-            <button className="button">
-              <span className="material-icons"> notifications </span>
-            </button>
-            <img
-              id="avatar"
-              src="https://social.webestica.com/assets/images/avatar/07.jpg"
-              alt=""
-            />
+          <div className="link">
+            <span>Account</span>
+            <span className="material-icons dropDown"> expand_more </span>
           </div>
+          <div className="link">
+            <span>My Network</span>
+          </div>
+
+          <button className="button">
+            <span className="material-icons"> chat </span>
+          </button>
+          <button className="button">
+            <span className="material-icons"> settings </span>
+          </button>
+          <button className="button">
+            <span className="material-icons"> notifications </span>
+          </button>
+          <img
+            id="avatar"
+            src="https://social.webestica.com/assets/images/avatar/07.jpg"
+            alt=""
+          />
         </div>
       </div>
     </div>
